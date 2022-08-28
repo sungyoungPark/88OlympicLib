5ecc0ab2ac915ab4e8652a6bbeccc936c11946ab
sungyoungPark <sygitter1491012@gmail.com>
2022-08-28 16:10:58 +0900

    Initial Commit

---
+++ 88OlympicLib/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  88OlympicLib
+//
+//  Created by 박성영 on 2022/08/28.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
